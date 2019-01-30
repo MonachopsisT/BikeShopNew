ef84aca9990e6a722e77deb17cc0bdf759fddd6e
MonachopsisT <teditod07@gmail.com>
2019-01-30 20:55:57 +0000

    Initial commit

---
+++ BikeShopNew/BikeShop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace BikeShopNew
+{
+    public class BikeShop
+    {
+        public int[,] myBikes;
+        public Product[] myProducts;
+
+        public BikeShop()
+        {
+            //bikeNumber,Price, Number of bikes of that type
+            Random rand = new Random();
+            
+            myProducts = new Product[rand.Next(1, 500)];
+
+            for (int i = 0; i < myProducts.Length; i++)
+            {
+                myProducts[i] = new Product(rand.Next(1,4), rand.Next(1,3));
+            }
+
+           // myBikes = new int[6, 3];
+           // for (int i = 0; i < 6; i++)
+           // {
+           //     myBikes[i, 0] = rand.Next(1, 4000000);
+            //    myBikes[i, 1] = ;
+            //    myBikes[i,2] = 
+
+
+        }
+        public void PrintStock()
+        {
+            for (int j = 0; j < myProducts.Length; j++)
+            {
+                myProducts[j].PrintDetails();
+            }
+            
+        }
+    }
+}
---
+++ BikeShopNew/Product.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace BikeShopNew
+{
+    public class Product
+    {
+        public string productType;
+        public int productTypeNum;//1-Off Road, 2-Racers, 3- Kids Carrier
+
+        public int productSystem; //1-electic, 2-mechanical
+        public string productSystemName; //1-electic, 2-mechanical
+
+        public double price;
+
+        Random rand = new Random();
+
+        public Product( int prodType, int prodSystem)
+        {
+            switch(prodType)
+            {
+                case 1:
+                    {
+                        productType = "OffRoad";
+                        price = 2.5;
+                    }
+                    break;
+                case 2:
+                    {
+                        productType = "Racer";
+                        price = 1.8;
+                    }
+                    break;
+                case 3:
+                    {
+                        productType = "KidsCarrier";
+                        price = 3;
+                    }
+                    break;
+                default:
+                    {
+                        productType = "Null";
+                        price = 0;  //price is zero if there is no such bike
+                        Console.WriteLine("Invalid input");
+                    }
+                    break;
+
+            }
+
+            if (prodSystem == 1)
+            {
+                productSystem = 1;
+                productSystemName = "Electric";
+                price += 1; //if it is electric add one to the price
+            }
+            else
+            {
+                productSystem = 2;
+                productSystemName = "Mechanical";
+            }
+
+        }
+       
+        public void PrintDetails()
+        {
+            Console.WriteLine("I am a: {0} and I am of type: {1} and my price is: {2}", productType, productSystemName, price);
+        }
+    }
+}
